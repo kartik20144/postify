cb8ff2a8479bfb9536d7e79c5ef622a6f6e3dbc1
Kartik <kartikagarwal20144@gmail.com>
2024-05-09 18:14:02 +0530

    fixing bugs

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+let BASE_URL = "https://postify-1.onrender.com/";
+https://postify-1.onrender.com/
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
